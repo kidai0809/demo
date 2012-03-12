bb0a27ca48d8cd54e1f9afec524e03237a15f8fa
Dai Huynh <kidai0809@gmail.com>
2012-03-12 11:48:21 +0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Demo::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
-
   resources :microposts
-
   resources :users
 
+  #Routing
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',    :to => 'users#new'
+
+  root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
